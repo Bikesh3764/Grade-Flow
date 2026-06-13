bbdc2a0d4cf82a1f742e0d6b3d2919a1459c12f1
VIKESH KUMAR RAY <125bt0022@nitrkl.ac.in>
2026-06-13 15:10:17 +0530

    perf: defer google analytics to lazyOnload to fix mobile TBT

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,12 +43,12 @@ export default function RootLayout({
       <head>
         {/* Google Analytics */}
         <Script
-          strategy="afterInteractive"
+          strategy="lazyOnload"
           src="https://www.googletagmanager.com/gtag/js?id=G-37PPH531JQ"
         />
         <Script
           id="google-analytics"
-          strategy="afterInteractive"
+          strategy="lazyOnload"
         >
           {`
             window.dataLayer = window.dataLayer || [];
