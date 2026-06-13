e216110f5333ef1dfd9af2063453d63b6770d1dd
VIKESH KUMAR RAY <125bt0022@nitrkl.ac.in>
2026-06-13 11:09:02 +0530

    fix(middleware): bypass supabase in middleware if env vars are missing

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -8,6 +8,11 @@ export async function updateSession(request: NextRequest) {
     },
   })
 
+  // Bypass if missing environment variables
+  if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+    return supabaseResponse;
+  }
+
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL || '',
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '',
